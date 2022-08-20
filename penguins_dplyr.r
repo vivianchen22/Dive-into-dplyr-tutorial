d0cebc1a07138866038512851e35a02edf676a06
Vivian22 <103799715+vivianchen22@users.noreply.github.com>
2022-08-20 17:32:07 +0800

    Update penguins_dplyr.r

--- penguins_dplyr.r
+++ penguins_dplyr.r
@@ -1,3 +1,4 @@
+# https://www.kaggle.com/code/jessemostipak/dive-into-dplyr-tutorial-1/notebook
 library('tidyverse')
 penguins <- read_csv('penguins_lter.csv')
 
@@ -7,9 +8,16 @@ head(penguins)
 summary(penguins)
 names(penguins)
 
+#[ Renames columns ]
+names(penguins) <- gsub("[[:punct:]]", " ", names(penguins))
+names(penguins) <- gsub("\\s\\s", "_", names(penguins))
+names(penguins) <- gsub("\\s", "_", names(penguins))
+names(penguins) <- gsub("[[:punct:]]$", "", names(penguins))
+
+
 #[ Exploring our data with dplyr ]
 penguins %>% filter(Island=='Torgersen') %>% head()
-penguins %>% arrange('Flipper Length') %>% head(10)
+penguins %>% arrange(Flipper_Length_mm) %>% head(10)
 
 #[ Creating a subset ]
 # creating a random subset of the penguins dataset
@@ -18,24 +26,24 @@ penguins_subset <- penguins %>% sample_n(12)
 penguins_subset %>% arrange(Species)
 
 #[ Nesting desc() inside arrange() ]
-penguins_subset %>% arrange(desc('Culmen Length (mm)'))
+penguins_subset %>% arrange(desc(Culmen_Length_mm))
 
 #[ Fun with filter() ]
 penguins_subset %>%
-  filter("Culmen Length (mm)" > 50)
+  filter(Culmen_Length_mm > 50)
 penguins_subset %>%
   filter(Island == "Dream")
 penguins_subset %>%
-  filter(between("Culmen Depth (mm)", 16.2, 18.1 ))
+  filter(between(Culmen_Length_mm, 16.2, 18.1 ))
 
 #[ Starting with select() ]
-penguins_subset %>% select(Island,"Culmen Depth (mm)")
+penguins_subset %>% select(Island,Culmen_Length_mm)
 penguins_subset %>% select(where(is.character))
 penguins_subset %>% select(where(is.numeric))
 penguins_subset %>% select(!where(is.numeric))
 
 #[ Math with mutate() ]
-penguins_subset <-penguins_subset %>% rename("Body_Mass_g"="Body Mass (g)")
+
 penguins_subset %>%
   mutate(Body_Mass_pound = Body_Mass_g / 453.59237) %>%
   select(Species, Body_Mass_g, Body_Mass_pound, everything())
@@ -44,7 +52,6 @@ penguins_subset %>%
 penguins_subset %>% summarise(avg_body_mass = mean(Body_Mass_g))
 
 #The NAs!
-penguins <- penguins %>% rename(Body_Mass_g ="Body Mass (g)")
 penguins %>% summarise (avg_body_mass = mean(Body_Mass_g))
 penguins %>% summarise (avg_body_mass = mean(Body_Mass_g,na.rm =TRUE))
 
@@ -53,3 +60,5 @@ penguins %>% group_by(Species) %>%
                        
 penguins %>% group_by(Species,Island) %>% 
   summarise(avg_body_mass = mean(Body_Mass_g,na.rm=TRUE))
+
+
